25231712ab10772ed2d49e74b9e475e8952de2a3
Luciano <majorass206@gmail.com>
2021-01-02 18:26:30 +0800

    設定子件素材/Skin底色

---
+++ react_tetris/src/gameHelpers.js
@@ -0,0 +1,8 @@
+//設定GAME範圍
+export const STAGE_WIDTH = 12;
+export const STAGE_HEIGHT = 20;
+
+export const createStage = () =>
+ Array.from(Array(STAGE_HEIGHT), () =>
+    new Array(STAGE_WIDTH).fill([0, 'clear'])
+ )
