56d0c998574501d244637e131fffe569f1542320
Luciano <majorass206@gmail.com>
2021-01-04 15:03:44 +0800

    row 滿消條

--- react_tetris/src/hooks/useStage.js
+++ react_tetris/src/hooks/useStage.js
@@ -7,8 +7,23 @@ import { createStage } from '../gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
     const [stage, setStage] = useState(createStage());
+    const [rowsCleared, setRowsCleared] = useState(0);
 
     useEffect(() => {
+        setRowsCleared(0);
+
+        //滿 row Fn
+        const sweepRows = newStage =>
+            newStage.reduce((ack, row) => {
+                if (row.findIndex(cell => cell[0] === 0) === -1) {
+                    setRowsCleared(prev => prev + 1);
+                    ack.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+                    return ack;
+                }
+                ack.push(row);
+                return ack;
+            }, [])
+
         const updateStage = prevStage => {
             //初入 stage
             const newStage = prevStage.map(row =>
@@ -29,6 +44,7 @@ export const useStage = (player, resetPlayer) => {
             // Then check if we collided
             if (player.collided) {
                 resetPlayer();
+                return sweepRows(newStage);
             }
 
             return newStage;
@@ -38,5 +54,5 @@ export const useStage = (player, resetPlayer) => {
 
     }, [player, resetPlayer]);
 
-    return [stage, setStage];
+    return [stage, setStage, rowsCleared];
 };
